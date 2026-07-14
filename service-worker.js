4c0a9803cea10cd389f666f88ae1296214197115
HKJPN <93431968+HKJPN@users.noreply.github.com>
2026-07-14 10:52:41 +0900

    Add PWA install prompt

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'ai-app-launcher-v1';
+const PRECACHE_URLS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './assets/launcher-person-cheek-arrow.png',
+  './assets/icons/icon-192.png',
+  './assets/icons/icon-512.png',
+  './assets/icons/apple-touch-icon.png',
+  './assets/icons/favicon-16.png',
+  './assets/icons/favicon-32.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(PRECACHE_URLS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => cachedResponse || fetch(event.request))
+  );
+});
